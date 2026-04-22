f19c9ee7d373c566787aadce9901f4abb3ab9349
Ahmed Elgendy <ahmedellgendy@outlook.com>
2026-04-22 14:35:01 +0200

    Add Customer Entity

---
+++ SalesOrderSystem.Domain/Entities/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace SalesOrderSystem.Domain.Entities
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Address { get; set; }
+        public required string Phone { get; set; }
+    }
+}
