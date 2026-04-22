3933f331fe523ebe147583dc31dd2ae828a52d22
Ahmed Elgendy <ahmedellgendy@outlook.com>
2026-04-22 14:48:56 +0200

    Add Order Entity

---
+++ SalesOrderSystem.Domain/Entities/Order.cs
@@ -0,0 +1,13 @@
+﻿namespace SalesOrderSystem.Domain.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public required string Status { get; set; }
+        public decimal TotalAmount { get; set; }
+        public Customer Customer { get; set; }
+        public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+    }
+}
